3f859e3f5eac7fe88f27d84859463f79a5c2a18b
Anuar <anuar.jimenez@itexico.com>
2020-03-02 10:50:22 -0600

    AJ: [ADD] Reselect for themes.

--- src/app/app.container.tsx
+++ src/app/app.container.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { IAppState } from '@rdx/root.reducer';
 
 import { Router } from 'react-router-dom';
-import { routerHistory } from '@routes/router.history';
 import { AppRoutes } from '@routes/app.routes';
+import { routerHistory } from '@routes/router.history';
 
+/* REDUX RELATED 0*/
 import { connect } from 'react-redux';
-
+import { getActiveTheme } from '@shared/themes/theme.selector';
+/* THEME PROVIDERS */
 import { ThemeProvider } from 'react-jss';
 import { configRootTheme } from '@themes/root.theme';
+
 import './normalize.styles.css';
 
 export interface IAppProps {
@@ -30,7 +33,7 @@ const App = (props: IAppProps) => {
 };
 
 const mapStateToProps = (state: IAppState) => ({
-  theme: state.theme.activeTheme,
+  theme: getActiveTheme(state),
 });
 
 export const AppContainer = connect(mapStateToProps)(App);
