afdc2aead018aa5ed964b2adeeeb8307eb596a2c
Anuar <anuar.jimenez@itexico.com>
2020-03-02 11:05:15 -0600

    AJ: [ADD] Redux routines utils.

--- src/shared/utils/redux-routines.ts
+++ src/shared/utils/redux-routines.ts
@@ -1,10 +1,10 @@
 import { RoutineReducer } from './redux-routines.d';
 
-function replaceEndpointPlaceholders(endpoint: any, params?: any) {
+function replaceEndpointPlaceholders(endpoint: string, params: any) {
   let renewedEndpoint = endpoint;
 
   if (params !== undefined && params !== null) {
-    Object.entries(params).forEach(element => {
+    Object.entries(params).forEach((element: any) => {
       const placeholder = `:${element[0]}`;
 
       renewedEndpoint = renewedEndpoint.replace(placeholder, element[1]);
@@ -16,34 +16,52 @@ function replaceEndpointPlaceholders(endpoint: any, params?: any) {
 
 function typeGenerator(type: string) {
   return {
-    TRIGGER: `${type}_TRIGGER`,
-    FULFILL: `${type}_FULFILL`,
-    REQUEST: `${type}_REQUEST`,
-    SUCCESS: `${type}_SUCCESS`,
-    FAILURE: `${type}_FAILURE`,
+    TRIGGER: `${type}/TRIGGER`,
+    FULFILL: `${type}/FULFILL`,
+    REQUEST: `${type}/REQUEST`,
+    SUCCESS: `${type}/SUCCESS`,
+    FAILURE: `${type}/FAILURE`,
   };
 }
 
-export function createActionRoutine(type: string, endpoint = '') {
+function createActionRoutine(type: string, endpoint = '') {
   const types = typeGenerator(type);
 
   return {
     ...types,
-    trigger: (payload: any = null) => ({ type: types.TRIGGER, payload }),
-    fulfill: (payload: any = null) => ({ type: types.FULFILL, payload }),
-    success: (payload: any = null) => ({ type: types.SUCCESS, payload }),
-    failure: (payload: any = null) => ({ type: types.FAILURE, payload }),
-    request: (payload: any = null) => ({ type: types.REQUEST, payload }),
-    getEndpoint: (params?: any) => replaceEndpointPlaceholders(endpoint, params),
+    trigger: (payload = null) => ({ type: types.TRIGGER, payload }),
+    fulfill: (payload = null) => ({ type: types.FULFILL, payload }),
+    success: (payload = null) => ({ type: types.SUCCESS, payload }),
+    failure: (payload = null) => ({ type: types.FAILURE, payload }),
+    request: (payload = null) => ({ type: types.REQUEST, payload }),
+    getEndpoint: (params: any) => replaceEndpointPlaceholders(endpoint, params),
   };
 }
 
-export function createRoutineReducer(): RoutineReducer {
+function createCrudReducer(params?: any): RoutineReducer {
   return {
     items: {},
     isLoading: false,
-    errorMessages: undefined,
+    errorMessages: {},
+    ...params,
   };
+}
+
+function createReducerRoutine(params: any) {
+  var modelBuffer = {};
+
+  params.forEach((item: any) => {
+    if (typeof item === 'string') {
+      modelBuffer = { ...modelBuffer, [item]: { ...createCrudReducer() } };
+    } else if (typeof item === 'object') {
+      modelBuffer = {
+        ...modelBuffer,
+        [item.key]: { ...createCrudReducer(item.model) },
+      };
+    }
+  });
+
+  return { ...modelBuffer };
 }
 
 export const getById = (dictionary: any, id: string | number) => {
